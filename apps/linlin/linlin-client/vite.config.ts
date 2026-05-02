794fb31d576cba6dc6098db98c11c35fcdb378ec
paul-clavier-nw <paul.clavier@nw-groupe.com>
2026-05-02 23:10:22 +0200

    feat: linlin client

---
+++ apps/linlin/linlin-client/vite.config.ts
@@ -0,0 +1,14 @@
+import { devtools } from "@tanstack/devtools-vite";
+import { defineConfig } from "vite";
+
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+
+import tailwindcss from "@tailwindcss/vite";
+import viteReact from "@vitejs/plugin-react";
+
+const config = defineConfig({
+    resolve: { tsconfigPaths: true },
+    plugins: [devtools(), tailwindcss(), tanstackStart(), viteReact()],
+});
+
+export default config;
